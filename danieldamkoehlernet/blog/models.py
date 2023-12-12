e96d79be0aa8d3e7aa4a5f2f8a7b1e1141b5c838
Daniel Damkoehler <170141+dandam@users.noreply.github.com>
2023-12-12 11:46:49 -0800

    reskin begins

--- danieldamkoehlernet/blog/models.py
+++ danieldamkoehlernet/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = (
-    (0, 'Draft'),
-    (1, 'Published')
-)
+STATUS = [
+    [0, 'Draft'],
+    [1, 'Published']
+]
 
 
 class Post(models.Model):
