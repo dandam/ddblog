7b001b22142f517bfa3016c022d73cccabaea7fc
Daniel Damkoehler <170141+dandam@users.noreply.github.com>
2023-12-12 17:39:26 -0800

    tailwind

--- danieldamkoehlernet/danieldamkoehlernet/settings.py
+++ danieldamkoehlernet/danieldamkoehlernet/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+# TODO: keep browser reload last in Middleware and remove entirely from production
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'danieldamkoehlernet.urls'
@@ -72,6 +78,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'danieldamkoehlernet.wsgi.application'
 
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- danieldamkoehlernet/danieldamkoehlernet/urls.py
+++ danieldamkoehlernet/danieldamkoehlernet/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# TODO: remove browser reload from production
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('__reload__/', include('django_browser_reload.urls')),
 ]
